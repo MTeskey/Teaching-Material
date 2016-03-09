a6885af60c2eff17e519475598b71a3001c051cd
areshenk <areshenk@uvic.ca>
2016-03-09 12:22:01 -0800

    Add corrected slides

---
+++ BayesWorkshop/1 - Likelihood and Bayesian Probability/code/binomial_model_fit.R
@@ -0,0 +1,26 @@
+setwd('PATH-TO-DATA')
+library(rstan)
+
+# Import data
+data = read.csv('coinflip_data.csv', header = F)
+
+# Create data file for stan
+stan_data = list(N = dim(data)[1],
+                 h = sum(data[,1]))
+
+# Fit stan model
+stan_fit = stan(file = 'binomial_model.stan',
+                data = stan_data)
+
+# Print model results
+print(stan_fit)
+
+# We can also extract the samples and plot the estimated posterior distribution, or
+# compute summary statistics
+stan_samples = extract(stan_fit)
+mean(stan_samples$p)
+hist(stan_samples$p,
+     probability = T, ylab = '',
+     xlab = 'p', main = 'Posterior distribution for p')
+abline(v = mean(stan_samples$p),
+       lty = 2, lwd = 2, col = 'red')
---
+++ BayesWorkshop/README.md
@@ -0,0 +1,13 @@
+# 2016 Bayes Tutorial
+Lecture slides, tutorial code, and models for an informal course on Bayesian modelling w/Stan aimed towards the social sciences. The intended prerequisites are some familiarity with basic statistics and general comfort with manipulating data, though I'm not sure how good a job I've done with keeping the level consistent.
+
+The material requires R:
+https://cran.r-project.org/
+
+...and RStan:
+https://github.com/stan-dev/rstan/wiki/RStan-Getting-Started
+
+RStudio is, of course, always helpful:
+https://www.rstudio.com/
+
+
